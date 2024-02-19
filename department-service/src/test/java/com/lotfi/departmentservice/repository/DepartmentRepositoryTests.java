0a4c07b8012b6c5812e8a8529d1b9cd31eed9d38
ofti01 <oftizeidi@gmail.com>
2024-02-19 21:29:42 +0100

    add unit test

--- department-service/src/test/java/com/lotfi/departmentservice/repository/DepartmentRepositoryTests.java
+++ department-service/src/test/java/com/lotfi/departmentservice/repository/DepartmentRepositoryTests.java
@@ -3,6 +3,7 @@ package com.lotfi.departmentservice.repository;
 import com.lotfi.departmentservice.model.Department;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -12,6 +13,7 @@ import java.util.List;
 @DataJpaTest
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class DepartmentRepositoryTests {
+    @Autowired
     private DepartmentRepository departmentRepository;
 
     @Test
